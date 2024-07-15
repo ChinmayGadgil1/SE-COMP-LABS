91edbfa623c177dc6dd5dfdc5a1c94e8b0529357
ChinmayGadgil1 <161110548+ChinmayGadgil1@users.noreply.github.com>
2024-07-15 21:16:53 +0530

    OOPS experiment for displaying the result

--- OOPS/studentGrade.cpp
+++ OOPS/studentGrade.cpp
@@ -1,111 +1,130 @@
-# include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
 
-class student{
-    public:
-    string first_name;
-    string last_name;
+class Student {
+public:
+    int rollNumber;
+    string name;
     int marks[4];
-    int GP;
-    float percentage;
-    bool passed;
 };
 
-int calcGP(int n){
-    int gp;
-    if (n >= 85)
-    {
-        gp = 10;
+char calculateGrade(int marks) {
+    if (marks >= 85) {
+        return 'O';
+    } else if (marks >= 75) {
+        return 'A';
+    } else if (marks >= 65) {
+        return 'B';
+    } else if (marks >= 55) {
+        return 'C';
+    } else if (marks >= 45) {
+        return 'D';
+    } else {
+        return 'F';
     }
-    else if (n >= 75)
-    {
-        gp = 9;
-    }
-
-    else if (n >= 65)
-    {
-        gp = 8;
-    }
-
-    else if (n >= 55)
-    {
-        gp = 7;
-    }
-
-    else if (n >= 50)
-    {
-        gp = 6;
-    }
-
-    else if (n >= 45)
-    {
-        gp = 5;
-    }
-
-    else if (n >= 40)
-    {
-        gp = 4;
-    }
-    else
-    {
-        gp = 0;
-    }
-    return gp;
 }
 
-
-int main(){
-int n;
-cout<<"Enter number of students: ";
-cin>>n;
-
-student s[n];
-
-for (int i = 0; i < n; i++)
-{
-    cout<<"\nEnter details of student "<<i+1<<" :"<<endl;
-    cout<<"Enter name: ";
-    cin>>s[i].first_name>>s[i].last_name;
-    cout<<"Enter marks:"<<endl;
-    cout<<"Math: ";
-    cin>>s[i].marks[0];
-    cout<<"Physics: ";
-    cin>>s[i].marks[1];
-    cout<<"ICE: ";
-    cin>>s[i].marks[2];
-    cout<<"Programming: ";
-    cin>>s[i].marks[3];
-    s[i].percentage=(float)(s[i].marks[0]+s[i].marks[1]+s[i].marks[2]+s[i].marks[3])/4.0;
-    s[i].GP=calcGP(s[i].percentage);
-    if (s[i].GP==0)
-    {
-        s[i].passed=false;
+int gradeToPoints(char grade) {
+    switch (grade) {
+        case 'O': return 10;
+        case 'A': return 9;
+        case 'B': return 8;
+        case 'C': return 7;
+        case 'D': return 6;
+        case 'F': return 0;
+        default: return 0;
     }
-    else
-    {
-        s[i].passed=true;
-    }
-    
-    
 }
 
-for (int i = 0; i < n; i++)
-{
-    cout<<"\nStudent "<<i+1<<endl;
-    cout<<"Name: "<<s[i].first_name<<" "<<s[i].last_name<<endl;
-    cout<<"Percentage: "<<s[i].percentage<<endl;
-    cout<<"Grade: "<<s[i].GP<<endl;
-    if (s[i].passed)
-    {
-        cout<<"Status: Student has passed"<<endl;
-    }
-    else
-    {
-        cout<<"Status: Student has Failed"<<endl;
-    }
-    
+void inputStudentData(Student students[], int index) {
+    cin.ignore();
+    cout << "\nEnter student's details for Student " << index + 1 << ":" << endl; 
+    cout << "Enter student's name: ";
+    getline(cin, students[index].name);
+
+    cout << "Enter student's roll number: ";
+    cin >> students[index].rollNumber;
+
+    cout << "Enter subjects and marks for semester:" << endl;
+    cout << "Enter math marks: ";
+    cin >> students[index].marks[0];
+    cout << "Enter physics marks: ";
+    cin >> students[index].marks[1];
+    cout << "Enter ICE marks: ";
+    cin >> students[index].marks[2];
+    cout << "Enter Prog. marks: ";
+    cin >> students[index].marks[3];
 }
 
+string passes(int marks) {
+    return marks < 40 ? "Fail" : "Pass";
+}
 
-return 0;
+void displayStudentData(Student students[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << "\nStudent Information:\n";
+        cout << "Name: " << students[i].name << endl;
+        cout << "Roll No: " << students[i].rollNumber << endl;
+        cout << "Subject  Marks  Grade Status" << endl;
+        cout << "-------------------" << endl;
+
+        for (int j = 0; j < 4; j++) {
+            
+            switch (j)
+            {
+            case 0:cout<<"Math     ";
+                break;
+            case 1:cout<<"Phy.     ";
+                break;
+            case 2:cout<<"ICE      ";
+                break;
+            case 3:cout<<"Prog     ";
+                break;
+            
+            }
+            cout << students[i].marks[j] << "     " 
+                 << calculateGrade(students[i].marks[j]) << "     " 
+                 << passes(students[i].marks[j]) << endl;
+        }
+    }
+}
+
+int main() {
+    int size;
+    cout << "Enter number of students: ";
+    cin >> size;
+    Student students[size];
+    int choice;
+
+    while (1) {
+        cout << "\nMenu:\n";
+        cout << "1. Add student data\n";
+        cout << "2. Display all results\n";
+        cout << "3. Quit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                    for (int i = 0; i < size; i++)
+                    {   
+                    inputStudentData(students, i);
+                    }
+                    
+            
+
+                break;
+            case 2:
+                displayStudentData(students, size);
+                break;
+            case 3:
+                cout << "Exiting..." << endl;
+                return 0;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+        }
+    }
+
+    return 0;
 }
