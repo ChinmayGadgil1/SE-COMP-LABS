a4fd0b4b92ab304402942397e7d757794d3dd615
ChinmayGadgil1 <161110548+ChinmayGadgil1@users.noreply.github.com>
2024-07-18 15:48:37 +0530

    ...

--- OOPS/chinmay/expt1BArrayOfObjects.cpp
+++ OOPS/chinmay/expt1BArrayOfObjects.cpp
@@ -2,122 +2,162 @@
 #include <iomanip>
 using namespace std;
 
-struct Student {
+class Student
+{
+private:
     int rollNumber;
     string name;
     int marks[4];
     int gp[4];
     double sgpa;
+
+public:
+    void inputStudentData(int);
+    char calculateGrade(int);
+    int calculateGradePoints(int);
+    string passes(int);
+    void displayStudentData();
 };
 
-char calculateGrade(int marks) {
-    if (marks >= 85) {
+char Student::calculateGrade(int marks)
+{
+    if (marks >= 85)
+    {
         return 'O';
-    } else if (marks >= 75) {
+    }
+    else if (marks >= 75)
+    {
         return 'A';
-    } else if (marks >= 65) {
+    }
+    else if (marks >= 65)
+    {
         return 'B';
-    } else if (marks >= 55) {
+    }
+    else if (marks >= 55)
+    {
         return 'C';
-    } else if (marks >= 45) {
+    }
+    else if (marks >= 45)
+    {
         return 'D';
-    } else {
+    }
+    else
+    {
         return 'F';
     }
 }
-int calculateGradePoints(int marks) {
-    if (marks >= 85 && marks<=100) {
+
+int Student::calculateGradePoints(int marks)
+{
+    if (marks >= 85 && marks <= 100)
+    {
         return 10;
-    } else if (marks >= 75) {
+    }
+    else if (marks >= 75)
+    {
         return 9;
-    } else if (marks >= 65) {
+    }
+    else if (marks >= 65)
+    {
         return 8;
-    } else if (marks >= 55) {
+    }
+    else if (marks >= 55)
+    {
         return 7;
-    } else if (marks >= 50) {
+    }
+    else if (marks >= 50)
+    {
         return 6;
-    } 
-    else if (marks>=45)
+    }
+    else if (marks >= 45)
     {
         return 5;
     }
-    else if (marks>=40)
+    else if (marks >= 40)
     {
         return 4;
     }
-    else {
+    else
+    {
         return 0;
     }
 }
 
-void inputStudentData(struct Student students[], int index) {
+void Student::inputStudentData(int index)
+{
     cin.ignore();
-    cout << "\nEnter student's details for Student " << index + 1 << ":" << endl; 
+    cout << "\nEnter student's details for Student " << index + 1 << ":" << endl;
     cout << "Enter student's name: ";
-    getline(cin, students[index].name);
+    getline(cin, name);
 
-    students[index].rollNumber=index+1;
+    rollNumber = index + 1;
 
     cout << "Enter subjects and marks for semester:" << endl;
     cout << "Enter math marks: ";
-    cin >> students[index].marks[0];
+    cin >> marks[0];
     cout << "Enter physics marks: ";
-    cin >> students[index].marks[1];
+    cin >> marks[1];
     cout << "Enter ICE marks: ";
-    cin >> students[index].marks[2];
+    cin >> marks[2];
     cout << "Enter Prog. marks: ";
-    cin >> students[index].marks[3];
-    students[index].gp[0]=calculateGradePoints(students[index].marks[0]);
-    students[index].gp[1]=calculateGradePoints(students[index].marks[1]);
-    students[index].gp[2]=calculateGradePoints(students[index].marks[2]);
-    students[index].gp[3]=calculateGradePoints(students[index].marks[3]);
+    cin >> marks[3];
+    gp[0] = calculateGradePoints(marks[0]);
+    gp[1] = calculateGradePoints(marks[1]);
+    gp[2] = calculateGradePoints(marks[2]);
+    gp[3] = calculateGradePoints(marks[3]);
 
-    students[index].sgpa=(double)(students[index].gp[0]*4+students[index].gp[1]*3+students[index].gp[2]*3+students[index].gp[3]*4)/13.0;
+    sgpa = (double)(gp[0] * 4 + gp[1] * 3 + gp[2] * 3 + gp[3] * 3) / 13.0;
+
+    
+
 }
 
-string passes(int marks) {
+string Student::passes(int marks)
+{
     return marks < 40 ? "Fail" : "Pass";
 }
 
-void displayStudentData(struct Student students[], int size) {
-    for (int i = 0; i < size; i++) {
-        cout << "\nStudent Information:\n";
-        cout << "Name: " << students[i].name << endl;
-        cout << "Roll No: " << students[i].rollNumber << endl;
-        cout << "Subject  Marks  Grade Status" << endl;
-        cout << "----------------------------" << endl;
-
-        for (int j = 0; j < 4; j++) {
-            
-            switch (j)
-            {
-            case 0:cout<<"Math     ";
-                break;
-            case 1:cout<<"Phy.     ";
-                break;
-            case 2:cout<<"ICE      ";
-                break;
-            case 3:cout<<"Prog     ";
-                break;
-            
-            }
-            cout << students[i].marks[j] << "     " 
-                 << calculateGrade(students[i].marks[j]) << "     " 
-                 << passes(students[i].marks[j]) << endl;
-
+void Student::displayStudentData()
+{
+    cout << "\nStudent Information:\n";
+    cout << "Name: " << name << endl;
+    cout << "Roll No: " << rollNumber << endl;
+    cout << "Subject  Marks  Grade Status" << endl;
+    cout << "----------------------------" << endl;
+    for (int j = 0; j < 4; j++)
+    {
+        switch (j)
+        {
+        case 0:
+            cout << "Math     ";
+            break;
+        case 1:
+            cout << "Phy.     ";
+            break;
+        case 2:
+            cout << "ICE      ";
+            break;
+        case 3:
+            cout << "Prog     ";
+            break;
         }
-        cout<<"SGPA: "<<setprecision(3)<<students[i].sgpa<<"\n";
+        cout << marks[j] << "     "
+             << calculateGrade(marks[j]) << "     "
+             << passes(marks[j]) << endl;
     }
+    cout << "SGPA: " << setprecision(3) << sgpa << "\n";
 }
 
-int main() {
+int main()
+{
     int size;
     cout << "Enter number of students: ";
     cin >> size;
-    struct Student student=new Student[size]
+    Student *students = new Student[size];
     int choice;
 
-    while (1) {
+    while (1)
+    {
         cout << "\nMenu:\n";
         cout << "1. Add student data\n";
         cout << "2. Display all results\n";
@@ -125,21 +165,29 @@ int main() {
         cout << "Enter your choice: ";
         cin >> choice;
 
-        switch (choice) {
-            case 1:
-                    for (int i = 0; i < size; i++)
-                    {   
-                    inputStudentData(students, i);
-                    }
-                break;
-            case 2:
-                displayStudentData(students, size);
-                break;
-            case 3:
-                cout << "Exiting..." << endl;
-                return 0;
-            default:
-                cout << "Invalid choice. Please try again." << endl;
+        if (choice == 1)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                students[i].inputStudentData(i);
+            }
+        }
+        else if (choice == 2)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                students[i].displayStudentData();
+            }
+        }
+        else if (choice == 3)
+        {
+            cout << "Exiting..." << endl;
+            delete[] students;  // Free allocated memory
+            return 0;
+        }
+        else
+        {
+            cout << "Invalid choice. Please try again." << endl;
         }
     }
 
