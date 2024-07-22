5bad491c5ccac8874b87cc49d55c92492bb530f1
ChinmayGadgil1 <161110548+ChinmayGadgil1@users.noreply.github.com>
2024-07-22 16:11:40 +0530

    Circular Queue

---
+++ DS/chinmay/EXPT7.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define MAX 4
+
+int front = -1, rear = -1;
+int cqueueArr[MAX];
+
+int isFull() {
+    return front>rear ||(front==0 && rear==MAX-1);
+}
+
+int isEmpty() {
+    return front == -1;
+}
+
+void insert(int item) {
+    if (isFull()) {
+        printf("Circular Queue Overflow\n");
+        return;
+    }
+    if (front==-1 && rear==-1) {
+        front = 0;
+        rear= 0;
+    }
+    
+    else
+    {
+     rear= (rear + 1) % MAX;
+    }
+    cqueueArr[rear] = item;
+}
+
+int delete() {
+    if (isEmpty()) {
+        printf("Circular Queue Underflow\n");
+        return INT_MIN;
+    }
+    int item = cqueueArr[front];
+    if (front == rear) {
+        front = rear = -1; 
+    }
+    else if (front==MAX-1)
+    {
+        front=0;
+    }
+    else{
+        front++;
+    }
+    
+    return item;
+}
+
+int peek() {
+    if (isEmpty()) {
+        printf("Circular Queue Underflow\n");
+        exit(1);
+    }
+    return cqueueArr[front];
+}
+
+void display(){
+    if (isEmpty())
+    {
+        printf("Circular Queue Empty\n");
+        return;
+    }
+    int i = front;
+    if (front<=rear)
+    {
+        while (i<=rear)
+        {
+            printf("%d ",cqueueArr[i]);
+            i++;
+        }
+        
+    }
+    else
+    {
+        while (i!=rear)
+        {
+            printf("%d ",cqueueArr[i]);
+            i=(i+1)%MAX;
+        }
+        printf("%d ",cqueueArr[i]);
+    }
+    
+    printf("\n");
+}
+
+int main() {
+    printf("\ninsert(64)\n");
+    insert(64);
+    display();
+    
+    printf("\ninsert(105)\n");
+    insert(105);
+    display();
+    
+    printf("\ninsert(100)\n");
+    insert(100);
+    display();
+    
+    printf("\ninsert(89)\n");
+    insert(89);
+    display();
+    
+    printf("\ninsert(100)\n");
+    insert(100);
+    display();
+    
+    printf("\ndelete()\n");
+    delete();
+    display();
+    
+    printf("\ndelete()\n");
+    delete();
+    display();
+    
+    printf("\ndelete()\n");
+    delete();
+    display();
+    
+    printf("\ndelete()\n");
+    delete();
+    display();
+    
+    printf("\ninsert(27)\n");
+    insert(27);
+    display();
+    
+    printf("\ndelete()\n");
+    delete();
+    display();
+    
+    printf("\ndelete()\n");
+    delete();
+    display();
+    
+    printf("\ninsert(45)\n");
+    insert(45);
+    display();  
+
+    return 0;
+}
+                                                                                                                                                                                   
