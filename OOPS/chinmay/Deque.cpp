d43bee527fe39d5a397ca6d78c4372d856a9e9e0
ChinmayGadgil1 <161110548+ChinmayGadgil1@users.noreply.github.com>
2024-08-08 15:25:40 +0530

    Search Function Implemented

--- OOPS/chinmay/Deque.cpp
+++ OOPS/chinmay/Deque.cpp
@@ -8,7 +8,7 @@ private:
     int front;
     int rear;
     int size;
-
+ 
 public:
     Deque()
     {
@@ -96,121 +96,125 @@ string Deque::pop_back()
     return pop;
 }
 
-void Deque::ForEach(){
+void Deque::ForEach()
+{
     if (isFrontEmpty())
     {
-        cout<<"\nFront Empty\n";
+        cout << "\nFront Empty\n";
     }
     else
     {
-        cout<<"\nArray from Front:\n";
+        cout << "\nArray from Front:\n";
         for (int i = 0; i <= front; i++)
         {
-            cout<<arr[i]<<"   ";
+            cout << arr[i] << "   ";
         }
-        cout<<"\n";
+        cout << "\n";
     }
     if (isRearEmpty())
     {
-        cout<<"\nRear Empty\n";
+        cout << "\nRear Empty\n";
     }
     else
     {
-        cout<<"\nArray from Rear:\n";
-        for (int i = size-1; i >=rear; i--)
+        cout << "\nArray from Rear:\n";
+        for (int i = size - 1; i >= rear; i--)
         {
-            cout<<arr[i]<<"   ";
+            cout << arr[i] << "   ";
         }
-        cout<<"\n";
+        cout << "\n";
     }
 }
 
-void Deque::FirstThat(string s){
+void Deque::FirstThat(string s)
+{
     if (!isFrontEmpty())
     {
         for (int i = 0; i <= front; i++)
         {
-            if(arr[i]==s){
-                cout<<"At index="<<i<<" found "<<arr[i]<<"\n";
+            if (arr[i] == s)
+            {
+                cout << "At index=" << i << " found " << arr[i] << "\n";
             }
         }
-        
     }
     if (!isRearEmpty())
     {
-        for (int i = size-1; i >=rear; i--)
+        for (int i = size - 1; i >= rear; i--)
         {
-            if(arr[i]==s){
-                cout<<"At index="<<i<<" found "<<arr[i]<<"\n";
+            if (arr[i] == s)
+            {
+                cout << "At index=" << i << " found " << arr[i] << "\n";
             }
         }
-        
     }
-    
 }
 
-void Deque::getLeft(){
+void Deque::getLeft()
+{
     if (isFrontEmpty())
     {
-        cout<<"\nDeque Empty from Front\n";
+        cout << "\nDeque Empty from Front\n";
         return;
     }
-    cout<<"\n";
+    cout << "\n";
     while (!isFrontEmpty())
     {
-        cout<<pop_front()<<"  ";
+        cout << pop_front() << "  ";
     }
-    cout<<"\n";
+    cout << "\n";
 }
-void Deque::getRight(){
+void Deque::getRight()
+{
     if (isRearEmpty())
     {
-        cout<<"\nDeque Empty from Rear \n";
+        cout << "\nDeque Empty from Rear \n";
         return;
     }
-    cout<<"\n";
+    cout << "\n";
     while (!isRearEmpty())
     {
-        cout<<pop_back()<<"  ";
+        cout << pop_back() << "  ";
     }
-    cout<<"\n";
+    cout << "\n";
 }
 
-void Deque::fill(){
+void Deque::fill()
+{
     int n;
-    cout<<"\nHow many elements would you like to push to front?\n";
-    cin>>n;
-    cout<<"\nEnter "<<n<<" strings\n";
+    cout << "\nHow many elements would you like to push to front?\n";
+    cin >> n;
+    cout << "\nEnter " << n << " strings\n";
     for (int i = 0; i < n; i++)
     {
         string push;
-        cin>>push;
+        cin >> push;
         push_front(push);
     }
 
-    cout<<"\nHow many elements would you like to push to back?\n";
-    cin>>n;
-    cout<<"\nEnter "<<n<<" strings\n";
+    cout << "\nHow many elements would you like to push to back?\n";
+    cin >> n;
+    cout << "\nEnter " << n << " strings\n";
     for (int i = 0; i < n; i++)
     {
         string push;
-        cin>>push;
+        cin >> push;
         push_back(push);
     }
-
-}  
+}
 
 int main()
 {
 
     Deque q;
     int choice;
-
+    string s;
+    
     while (true)
-    {   
-        cout<<"\n1.Fill\n2.Display\n3.getLeft\n4.getRight\n5.Exit\n";
-        cout<<"Enter your choice=> ";
-        cin>>choice;
+    {
+        cout << "\n1.Fill\n2.Display\n3.getLeft\n4.getRight\n5.Search\n6.Exit\n";
+        cout << "Enter your choice=> ";
+        cin >> choice;
         switch (choice)
         {
         case 1:
@@ -226,6 +230,11 @@ int main()
             q.getRight();
             break;
         case 5:
+            cout<<"\nEnter string to search: ";
+            cin>>s;
+            q.FirstThat(s);
+            break;
+        case 6:
             exit(0);
         default:
             break;
