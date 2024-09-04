dc8a53832d9f4a1c506c6f7410abca80cc4cfb29
ChinmayGadgil1 <161110548+ChinmayGadgil1@users.noreply.github.com>
2024-09-04 14:58:28 +0530

    In threaded BST

--- DS/chinmay/In-threaded-bst.c
+++ DS/chinmay/In-threaded-bst.c
@@ -105,13 +105,121 @@ void deleteAllocation(struct node* root){
     }
 }
 
+struct node* in_succ(struct node* ptr){
+    if(ptr->rthread==true)
+        return ptr->right;
+    ptr=ptr->right;
+    while(ptr->lthread==false)
+        ptr=ptr->left;
+    return ptr;
+}
+
+struct node* in_pred(struct node* ptr){
+    if(ptr->lthread==true)
+        return ptr->left;
+    ptr=ptr->left;
+    while(ptr->rthread==false)
+        ptr=ptr->right;
+    return ptr;
+}  
+
+struct node* case_a(struct node* root,struct node* par,struct node* ptr){
+    if(par==NULL)
+        root=NULL;
+    else if(ptr==par->left){
+        par->lthread=true;
+        par->left=ptr->left;
+    }
+    else{
+        par->rthread=true;
+        par->right=ptr->right;
+    }
+    free(ptr);
+    return root;
+}
+
+struct node* case_b(struct node* root,struct node* par,struct node* ptr){
+    struct node* child,*s,*p;
+    if(ptr->lthread==false)
+        child=ptr->left;
+    else
+        child=ptr->right;
+    if(par==NULL)
+        root=child;
+    else if(ptr==par->left)
+        par->left=child;
+    else
+        par->right=child;
+    s=in_succ(ptr);
+    p=in_pred(ptr);
+    if(ptr->lthread==false)
+        p->right=s;
+    else{
+        if(ptr->rthread==false)
+            s->left=p;
+    }
+    free(ptr);
+    return root;
+}
+
+struct node* case_c(struct node* root,struct node* par,struct node* ptr){
+    struct node* succ,*parsucc;
+    parsucc=ptr;
+    succ=ptr->right;
+    while(succ->left!=NULL){
+        parsucc=succ;
+        succ=succ->left;
+    }
+    ptr->info=succ->info;
+    if(succ->lthread==true && succ->rthread==true)
+        root=case_a(root,parsucc,succ);
+    else
+        root=case_b(root,parsucc,succ);
+    return root;
+}
+
+struct node* delete(struct node* root,int dkey){
+    struct node* ptr=root,*par=NULL,*child,*succ,*parsucc;
+    bool found=false;
+    while(ptr!=NULL){
+        if(dkey==ptr->info){
+            found=true;
+            break;
+        }
+        par=ptr;
+        if(dkey<ptr->info){
+            if(ptr->lthread==false)
+                ptr=ptr->left;
+            else
+                break;
+        }
+        else{
+            if(ptr->rthread==false)
+                ptr=ptr->right;
+            else
+                break;
+        }
+    }
+    if(!found){
+        printf("Key not found\n");
+        return root;
+    }
+    else if(ptr->lthread==false && ptr->rthread==false)
+        root=case_c(root,par,ptr);
+    else if(ptr->lthread==false || ptr->rthread==false)
+        root=case_b(root,par,ptr);
+    else
+        root=case_a(root,par,ptr);
+    return root;
+}
+
 int main(){
 
     struct node* root=NULL;
     int choice,ikey;
     struct node* ptr;
     while(1){
-        printf("1.Insert\n2.Search\n3.Exit\n");
+        printf("1.Insert\n2.Search\n3.Delete\n4.Exit\n");
         scanf("%d",&choice);
         switch(choice){
             case 1:
@@ -127,7 +235,14 @@ int main(){
                     printf("\nKey found\n\n");
                 break;
             case 3:
+                printf("Enter the key to delete:");
+                scanf("%d",&ikey);
+                root=delete(root,ikey);
+                break;
+            case 4:
                 exit(0);
+            default:
+                printf("Invalid choice\n");
         }
     }
 
