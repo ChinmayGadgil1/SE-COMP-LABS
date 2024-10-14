c155e35583928a199b8278789e28570f37cf93c8
ChinmayGadgil1 <161110548+ChinmayGadgil1@users.noreply.github.com>
2024-10-14 15:26:41 +0530

    hashing

--- DS/chinmay/non-linear/hashing.c
+++ DS/chinmay/non-linear/hashing.c
@@ -22,7 +22,7 @@ int hash(int key){
     return key%MAX;
 }
 
-void insert(struct Employee emp,struct Record table[]){
+void insertLinear(struct Employee emp,struct Record table[]){
     int i,loc,h;
     int key=emp.empid;
     h=hash(key);
@@ -57,6 +57,49 @@ int search(int key,struct Record table[]){
     return -1;
 }
 
+void insertQuadratic(struct Employee emp,struct Record table[]){
+    int i,loc,h;
+    int key=emp.empid;
+    h=hash(key);
+    loc=h;
+    for(i=1;i!=MAX-1;i++){
+        if(table[loc].status==EMPTY || table[loc].status==DELETED){
+            table[loc].info=emp;
+            table[loc].status=OCCUPIED;
+            return;
+        }
+        if(table[loc].info.empid==key){
+            printf("Duplicate key\n");
+            return;
+        }
+        loc=(h+i*i)%MAX;
+    }
+}
+
+int hash2(int key){
+    return 7-(key%7);
+}
+
+void insertDoubleHash(struct Employee emp,struct Record table[]){
+    int i,loc,h,h2;
+    int key=emp.empid;
+    h=hash(key);
+    h2=hash2(key);
+    loc=h;
+    for(i=1;i!=MAX-1;i++){
+        if(table[loc].status==EMPTY || table[loc].status==DELETED){
+            table[loc].info=emp;
+            table[loc].status=OCCUPIED;
+            return;
+        }
+        if(table[loc].info.empid==key){
+            printf("Duplicate key\n");
+            return;
+        }
+        loc=(h+i*h2)%MAX;
+    }
+}
+
 void delete(int key,struct Record table[]){
     int loc=search(key,table);
     if(loc==-1){
@@ -76,6 +119,12 @@ void display(struct Record table[]){
             printf("Name: %s\n",table[i].info.name);
             printf("Age: %d\n",table[i].info.age);
         }
+        else if(table[i].status==DELETED){
+            printf("Record deleted\n");
+        }
+        else{
+            printf("Empty record\n");
+        }
     }
 }
 
@@ -85,8 +134,15 @@ int main(){
         table[i].status = EMPTY;
     }
 
-    int choice, key;
+    int choice, key, hashType;
     struct Employee emp;
+
+    printf("Choose type of hashing:\n");
+    printf("1. Linear Probing\n");
+    printf("2. Quadratic Probing\n");
+    printf("3. Double Hashing\n");
+    printf("Enter your choice: ");
+    scanf("%d", &hashType);
 
     while (1) {
         printf("\nMenu:\n");
@@ -106,7 +162,15 @@ int main(){
                 scanf("%s", emp.name);
                 printf("Enter Age: ");
                 scanf("%d", &emp.age);
-                insert(emp, table);
+                if (hashType == 1) {
+                    insertLinear(emp, table);
+                } else if (hashType == 2) {
+                    insertQuadratic(emp, table);
+                } else if (hashType == 3) {
+                    insertDoubleHash(emp, table);
+                } else {
+                    printf("Invalid hash type\n");
+                }
                 break;
             case 2:
                 printf("Enter Employee ID to search: ");
@@ -132,7 +196,6 @@ int main(){
                 printf("Invalid choice\n");
         }
     }
-                
 
 
 return 0;
