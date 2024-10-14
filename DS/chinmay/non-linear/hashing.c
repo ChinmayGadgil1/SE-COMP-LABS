01c6f34dd06cc4298d619ded144c4d1be2ae97be
ChinmayGadgil1 <161110548+ChinmayGadgil1@users.noreply.github.com>
2024-10-14 15:26:53 +0530

    hashing

--- DS/chinmay/non-linear/hashing.c
+++ DS/chinmay/non-linear/hashing.c
@@ -1,141 +1,157 @@
-# include<stdio.h>
+#include <stdio.h>
 #define MAX 7
 
-enum type_of_record{
+enum type_of_record
+{
     EMPTY,
     DELETED,
     OCCUPIED
 };
 
-struct Employee{
-    int empid;
-    char name[20];
-    int age;
-};
-
-struct Record{
-    struct Employee info;
+struct Record
+{
+    int info;
     enum type_of_record status;
 };
 
-int hash(int key){
-    return key%MAX;
+int hash(int key)
+{
+    return key % MAX;
 }
 
-void insertLinear(struct Employee emp,struct Record table[]){
-    int i,loc,h;
-    int key=emp.empid;
-    h=hash(key);
-    loc=h;
-    for(i=1;i!=MAX-1;i++){
-        if(table[loc].status==EMPTY || table[loc].status==DELETED){
-            table[loc].info=emp;
-            table[loc].status=OCCUPIED;
+void insertLinear(int ikey, struct Record table[])
+{
+    int i, loc, h;
+    h = hash(ikey);
+    loc = h;
+    for (i = 1; i != MAX - 1; i++)
+    {
+        if (table[loc].status == EMPTY || table[loc].status == DELETED)
+        {
+            table[loc].info = ikey;
+            table[loc].status = OCCUPIED;
             return;
         }
-        if(table[loc].info.empid==key){
+        if (table[loc].info == ikey)
+        {
             printf("Duplicate key\n");
             return;
         }
-        loc=(h+i)%MAX;
+        loc = (h + i) % MAX;
     }
 }
 
-int search(int key,struct Record table[]){
-    int i,loc,h;
-    h=hash(key);
-    loc=h;
-    for(i=1;i!=MAX-1;i++){
-        if(table[loc].status==EMPTY){
+int search(int key, struct Record table[])
+{
+    int i, loc, h;
+    h = hash(key);
+    loc = h;
+    for (i = 1; i != MAX - 1; i++)
+    {
+        if (table[loc].status == EMPTY)
+        {
             return -1;
         }
-        if(table[loc].status==OCCUPIED && table[loc].info.empid==key){
+        if (table[loc].status == OCCUPIED && table[loc].info == key)
+        {
             return loc;
         }
-        loc=(h+i)%MAX;
+        loc = (h + i) % MAX;
     }
     return -1;
 }
 
-void insertQuadratic(struct Employee emp,struct Record table[]){
-    int i,loc,h;
-    int key=emp.empid;
-    h=hash(key);
-    loc=h;
-    for(i=1;i!=MAX-1;i++){
-        if(table[loc].status==EMPTY || table[loc].status==DELETED){
-            table[loc].info=emp;
-            table[loc].status=OCCUPIED;
+void insertQuadratic(int ikey, struct Record table[])
+{
+    int i, loc, h;
+    h = hash(ikey);
+    loc = h;
+    for (i = 1; i != MAX - 1; i++)
+    {
+        if (table[loc].status == EMPTY || table[loc].status == DELETED)
+        {
+            table[loc].info = ikey;
+            table[loc].status = OCCUPIED;
             return;
         }
-        if(table[loc].info.empid==key){
+        if (table[loc].info == ikey)
+        {
             printf("Duplicate key\n");
             return;
         }
-        loc=(h+i*i)%MAX;
+        loc = (h + i * i) % MAX;
     }
 }
 
-int hash2(int key){
-    return 7-(key%7);
+int hash2(int key)
+{
+    return 7 - (key % 7);
 }
 
-void insertDoubleHash(struct Employee emp,struct Record table[]){
-    int i,loc,h,h2;
-    int key=emp.empid;
-    h=hash(key);
-    h2=hash2(key);
-    loc=h;
-    for(i=1;i!=MAX-1;i++){
-        if(table[loc].status==EMPTY || table[loc].status==DELETED){
-            table[loc].info=emp;
-            table[loc].status=OCCUPIED;
+void insertDoubleHash(int ikey, struct Record table[])
+{
+    int i, loc, h, h2;
+    h = hash(ikey);
+    h2 = hash2(ikey);
+    loc = h;
+    for (i = 1; i != MAX - 1; i++)
+    {
+        if (table[loc].status == EMPTY || table[loc].status == DELETED)
+        {
+            table[loc].info = ikey;
+            table[loc].status = OCCUPIED;
             return;
         }
-        if(table[loc].info.empid==key){
+        if (table[loc].info == ikey)
+        {
             printf("Duplicate key\n");
             return;
         }
-        loc=(h+i*h2)%MAX;
+        loc = (h + i * h2) % MAX;
     }
 }
 
-void delete(int key,struct Record table[]){
-    int loc=search(key,table);
-    if(loc==-1){
+void delete(int key, struct Record table[])
+{
+    int loc = search(key, table);
+    if (loc == -1)
+    {
         printf("Record not found\n");
         return;
     }
-    table[loc].status=DELETED;
+    table[loc].status = DELETED;
     printf("Record deleted\n");
 }
 
-void display(struct Record table[]){
-    for(int i=0;i!=MAX;i++){
-        printf("Record: %d\n",i);
-        if(table[i].status==OCCUPIED){
-            printf("Location: %d\n",i);
-            printf("Employee ID: %d\n",table[i].info.empid);
-            printf("Name: %s\n",table[i].info.name);
-            printf("Age: %d\n",table[i].info.age);
+void display(struct Record table[])
+{
+    for (int i = 0; i < MAX; i++)
+    {
+        printf("Record: %d =>", i);
+        if (table[i].status == OCCUPIED)
+        {
+            printf("Key: %d\n", table[i].info);
         }
-        else if(table[i].status==DELETED){
+        else if (table[i].status == DELETED)
+        {
             printf("Record deleted\n");
         }
-        else{
+        else
+        {
             printf("Empty record\n");
         }
     }
 }
 
-int main(){
+int main()
+{
     struct Record table[MAX];
-    for (int i = 0; i < MAX; i++) {
+    for (int i = 0; i < MAX; i++)
+    {
         table[i].status = EMPTY;
     }
 
     int choice, key, hashType;
-    struct Employee emp;
 
     printf("Choose type of hashing:\n");
     printf("1. Linear Probing\n");
@@ -144,7 +160,8 @@ int main(){
     printf("Enter your choice: ");
     scanf("%d", &hashType);
 
-    while (1) {
+    while (1)
+    {
         printf("\nMenu:\n");
         printf("1. Insert\n");
         printf("2. Search\n");
@@ -154,49 +171,55 @@ int main(){
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
-        switch (choice) {
-            case 1:
-                printf("Enter Employee ID: ");
-                scanf("%d", &emp.empid);
-                printf("Enter Name: ");
-                scanf("%s", emp.name);
-                printf("Enter Age: ");
-                scanf("%d", &emp.age);
-                if (hashType == 1) {
-                    insertLinear(emp, table);
-                } else if (hashType == 2) {
-                    insertQuadratic(emp, table);
-                } else if (hashType == 3) {
-                    insertDoubleHash(emp, table);
-                } else {
-                    printf("Invalid hash type\n");
-                }
-                break;
-            case 2:
-                printf("Enter Employee ID to search: ");
-                scanf("%d", &key);
-                int loc = search(key, table);
-                if (loc == -1) {
-                    printf("Record not found\n");
-                } else {
-                    printf("Record found at location %d\n", loc);
-                }
-                break;
-            case 3:
-                printf("Enter Employee ID to delete: ");
-                scanf("%d", &key);
-                delete(key, table);
-                break;
-            case 4:
-                display(table);
-                break;
-            case 5:
-                return 0;
-            default:
-                printf("Invalid choice\n");
+        switch (choice)
+        {
+        case 1:
+            printf("Enter Key: ");
+            scanf("%d", &key);
+            if (hashType == 1)
+            {
+                insertLinear(key, table);
+            }
+            else if (hashType == 2)
+            {
+                insertQuadratic(key, table);
+            }
+            else if (hashType == 3)
+            {
+                insertDoubleHash(key, table);
+            }
+            else
+            {
+                printf("Invalid hash type\n");
+            }
+            break;
+        case 2:
+            printf("Enter Employee ID to search: ");
+            scanf("%d", &key);
+            int loc = search(key, table);
+            if (loc == -1)
+            {
+                printf("Record not found\n");
+            }
+            else
+            {
+                printf("Record found at location %d\n", loc);
+            }
+            break;
+        case 3:
+            printf("Enter Employee ID to delete: ");
+            scanf("%d", &key);
+            delete (key, table);
+            break;
+        case 4:
+            display(table);
+            break;
+        case 5:
+            return 0;
+        default:
+            printf("Invalid choice\n");
         }
     }
 
-
-return 0;
+    return 0;
 }
